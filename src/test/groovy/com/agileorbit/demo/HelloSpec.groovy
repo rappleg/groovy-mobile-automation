05a6708f60c0503dd0e801f97ea94c5050870a38
Bobby Warner <bobbywarner@gmail.com>
2014-09-07 19:55:26 -0500

    Craeted data-driven Spock test using Unroll

--- src/test/groovy/com/agileorbit/demo/HelloSpec.groovy
+++ src/test/groovy/com/agileorbit/demo/HelloSpec.groovy
@@ -1,17 +1,33 @@
 package com.agileorbit.demo
 
+import spock.lang.Unroll
+
 class HelloSpec extends BaseSpec {
 
-    def "say hello in spanish"() {
+    @Unroll
+    def "say hello to #english in spanish"(String english, String spanish) {
         given:
         def input = h.text()
         def button = h.name("Send")
 
-        when:
-        input.sendKeys("Bobby")
+        expect:
+        input.sendKeys(english)
         button.click()
+        h.text("Hola $spanish") != null
 
-        then:
-        h.text("Hola Roberto") != null
+        where:
+        english     | spanish
+        "Bobby"     | "Roberto"
+        "George"    | "Jorge"
+        "Alexander" | "Alejandro"
+        "Mark"      | "Marcos"
+        "John"      | "Juan"
+        "Steve"     | "Esteban"
+        "Doug"      | "Diego"
+        "Henry"     | "Enrique"
+        "Michael"   | "Miguel"
+        "Peter"     | "Pedro"
+        "Mary"      | "Maria"
+        "Rachel"    | "Raquel"
     }
 }
