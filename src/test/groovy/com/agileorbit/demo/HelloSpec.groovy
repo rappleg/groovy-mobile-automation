c6c73c658a084d3bcb1777e0784d70bea8e1a24c
Bobby Warner <bobbywarner@gmail.com>
2014-09-09 16:18:58 -0500

    Added test for invalid name

--- src/test/groovy/com/agileorbit/demo/HelloSpec.groovy
+++ src/test/groovy/com/agileorbit/demo/HelloSpec.groovy
@@ -2,7 +2,7 @@ package com.agileorbit.demo
 
 class HelloSpec extends BaseSpec {
 
-    def "say hello in spanish"() {
+    def "say hello in spanish with name"() {
         given:
         def input = h.text()
         def button = h.name("Send")
@@ -14,4 +14,17 @@ class HelloSpec extends BaseSpec {
         then:
         h.text("Hola Roberto") != null
     }
+
+    def "say hello in spanish with invalid name"() {
+        given:
+        def input = h.text()
+        def button = h.name("Send")
+
+        when:
+        input.sendKeys("foobar")
+        button.click()
+
+        then:
+        h.text("Hola Usted") != null
+    }    
 }
