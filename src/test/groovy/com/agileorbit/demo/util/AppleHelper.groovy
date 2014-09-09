650d4610b9fe668f41d6613e5c44997262348dbd
Bobby Warner <bobbywarner@gmail.com>
2014-09-09 16:28:11 -0500

    Refactored helpers to use traits

--- src/test/groovy/com/agileorbit/demo/util/AppleHelper.groovy
+++ src/test/groovy/com/agileorbit/demo/util/AppleHelper.groovy
@@ -12,63 +12,27 @@ class AppleHelper implements Helper {
         driver = webDriver
     }
 
-    MobileElement element(By locator) {
-        driver.findElement(locator)
-    }
-
-    List<MobileElement> elements(By locator) {
-        driver.findElements(locator)
-    }
-
-    void back() {
-        driver.navigate().back()
-    }
-
-    List<MobileElement> tags(String tagName) {
-        elements(forTags(tagName))
-    }
-
-    By forTags(String tagName) {
-        By.className(tagName)
-    }
-
-    MobileElement name(String text) {
-        element(By.name(text))
-    }
-
     MobileElement text() {
         element(By.className("UIATextField"))
     }
 
     MobileElement text(int xpathIndex) {
-        element(forText(xpathIndex))
-    }
-
-    By forText(int xpathIndex) {
-        By.xpath("//UIAStaticText[" + xpathIndex + "]")
+        element(By.xpath("//UIAStaticText[" + xpathIndex + "]"))
     }
 
     MobileElement text(String text) {
-        element(forText(text))
-    }
-
-    By forText(String text) {
-        String up = text.toUpperCase();
-        String down = text.toLowerCase();
-        By.xpath("//UIAStaticText[@visible=\"true\" and (contains(translate(@name,\"" + up
+        String up = text.toUpperCase()
+        String down = text.toLowerCase()
+        element(By.xpath("//UIAStaticText[@visible=\"true\" and (contains(translate(@name,\"" + up
                 + "\",\"" + down + "\"), \"" + down + "\") or contains(translate(@hint,\"" + up
                 + "\",\"" + down + "\"), \"" + down + "\") or contains(translate(@label,\"" + up
                 + "\",\"" + down + "\"), \"" + down + "\") or contains(translate(@value,\"" + up
-                + "\",\"" + down + "\"), \"" + down + "\"))]")
+                + "\",\"" + down + "\"), \"" + down + "\"))]"))
     }
 
     MobileElement textExact(String text) {
-        element(forTextExact(text))
-    }
-
-    By forTextExact(String text) {
-        By.xpath("//UIAStaticText[@visible=\"true\" and (@name=\"" + text
+        element(By.xpath("//UIAStaticText[@visible=\"true\" and (@name=\"" + text
                 + "\" or @hint=\"" + text + "\" or @label=\"" + text
-                + "\" or @value=\"" + text + "\")]")
+                + "\" or @value=\"" + text + "\")]"))
     }
 }
