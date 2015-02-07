9bef02034bb2f59097f8c4d3694f7d2b662f1396
Bobby Warner <bobbywarner@gmail.com>
2015-02-06 20:38:24 -0600

    Fix iOS version

--- src/test/groovy/com/agileorbit/demo/BaseSpec.groovy
+++ src/test/groovy/com/agileorbit/demo/BaseSpec.groovy
@@ -24,7 +24,7 @@ abstract class BaseSpec extends Specification {
                 File app = new File(appDir, "DataEntryDemo.app")
                 DesiredCapabilities capabilities = new DesiredCapabilities()
                 capabilities.setCapability(MobileCapabilityType.BROWSER_NAME, "")
-                capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "7.1")
+                capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "8.1")
                 capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, MobilePlatform.IOS)
                 capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone Simulator")
                 capabilities.setCapability(MobileCapabilityType.APP, app.getAbsolutePath())
